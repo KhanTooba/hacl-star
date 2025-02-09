b0ff493bdfce564de49bc4da6657b4c60a400283
KhanTooba <tooba.khan@usc.edu>
2025-02-08 19:09:10 -0800

    Made changes to be compatible for RISCV

---
+++ dist/gcc-compatible/compilationCommands.sh
@@ -0,0 +1,1 @@
+sudo clang-14 -march=rv64gc -O0 -S -g -emit-llvm Hacl_HMAC.c -I ../karamel/include/ -I ../karamel/krmllib/dist/minimal/ -c -o hmac.ll --target=riscv64 -mabi=lp64d -I /usr/lib/llvm-14/lib/clang/14.0.6/include/riscv_vector.h -I /usr/riscv64-linux-gnu/include/
